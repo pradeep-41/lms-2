29755c06fa1dd0ef2770ef637d860d77ef3aaa72
pradeep-41 <pradeep@resimator.fi>
2025-06-17 17:31:06 +0545

    small change on applicationlist component

--- src/app/(protected)/applications/page.tsx
+++ src/app/(protected)/applications/page.tsx
@@ -1,11 +1,10 @@
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 import React from 'react'
 import { ApplicationsLists } from '../../../components/applicationsItems/ApplicationLists';
 
 export default function page() {
   return (
     <View className='flex-1 justify-center bg-white'>
-      <Text>application page</Text>
       <ApplicationsLists />
     </View>
   )
--- src/components/applicationsItems/ApplicationLists.tsx
+++ src/components/applicationsItems/ApplicationLists.tsx
@@ -1,30 +1,33 @@
-import { ScrollView, View } from "react-native";
+import { ScrollView } from "react-native";
 import { Button } from "../button/Button";
 
 export const ApplicationsLists = () => {
-    const filterValue = [
-      { id: "all", value: "All" }, 
-      { id: "pending", value: "Pending" },
-      { id: "granted", value: "Granted" },
-      { id: "rejected", value: "Rejected" },
-      { id: "cancelled", value: "Cancelled" }
-    ]
+  const filterValue = [
+    { id: "all", value: "All" },
+    { id: "pending", value: "Pending" },
+    { id: "granted", value: "Granted" },
+    { id: "rejected", value: "Rejected" },
+    { id: "cancelled", value: "Cancelled" },
+  ];
+
   return (
     <ScrollView
       horizontal
       className="p-4 w-full"
-   
       showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{
+        paddingRight: 12, 
+      }}
     >
       {filterValue.map((val) => (
-        <View
+        <Button
           key={val.id}
-          className="h-12 mr-4 bg-cyan-100 rounded-lg justify-center items-center"
+          variant="outline"
+          colorScheme="primary"
+          className="mr-4 h-[8%] px-4 py-2 " 
         >
-          <Button >
-            {val.value}
-          </Button>
-        </View>
+          {val.value}
+        </Button>
       ))}
     </ScrollView>
   );
